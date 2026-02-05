952931a97a6d654ee50004e1e6a0acfeb022dd36
morgandowney-droid <morgan.downey@gmail.com>
2026-02-05 14:08:14 +0100

    Revert Stripe API version - required by stripe@20.2.0

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -8,7 +8,7 @@ export function getStripe(): Stripe {
       throw new Error('STRIPE_SECRET_KEY is not set');
     }
     stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: '2024-06-20',
+      apiVersion: '2025-12-15.clover',
       typescript: true,
     });
   }
